c33eb2187c3a729378b3e641bb448ee70b8a7fb0
lanpodder <alan.i.shref@gmail.com>
2019-10-14 12:57:17 +0200

    error case

---
+++ NotAMotor.py
@@ -0,0 +1,6 @@
+class NotAMotor:
+    def __init__(self):
+        pass
+    
+    def notMotorThings(self):
+        print("im not a motor")
--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from Car import Car
 from ElectroMotor import ElectroMotor
 from DieselMotor import DieselMotor
+from NotAMotor import NotAMotor
 
 if __name__ == "__main__":
     electricMotor = ElectroMotor()
     dieselMotor = DieselMotor()
+    notAMotor = NotAMotor()
 
     electricCar = Car(electricMotor)
     electricCar.drive()
 
     dieselCar = Car(dieselMotor)
     dieselCar.drive()
+
+    notWorkingcar = Car(notAMotor)
+    notWorkingcar.drive()
